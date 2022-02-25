e9bfb1c2d1ec7e4b51f5a29bc7035d0d800cd4cc
Kofi Boateng <kdboat2@gmail.com>
2022-02-24 22:42:01 -0600

    Pushing Legacy Banking Backend

---
+++ src/main/java/com/legacybanking/legacyBankingAPI/LegacyBankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.legacybanking.legacyBankingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyBankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyBankingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legacybanking/legacyBankingAPI/LegacyBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legacybanking.legacyBankingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyBankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
