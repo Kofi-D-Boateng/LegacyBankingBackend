8e467528b84dcadc37cb05dac0c11c9fe1bed17b
Kofi Boateng <kdboat2@gmail.com>
2022-09-08 13:44:42 -0500

    added mock test for bank service.

--- src/main/java/com/legacybanking/legacyBankingAPI/Interfaces/ConfirmationTokenInterface.java
+++ src/main/java/com/legacybanking/legacyBankingAPI/Interfaces/ConfirmationTokenInterface.java
@@ -3,7 +3,6 @@ package com.legacybanking.legacyBankingAPI.Interfaces;
 import com.legacybanking.legacyBankingAPI.models.VerificationToken;
 
 public interface ConfirmationTokenInterface {
-    void saveToken(VerificationToken token);
     VerificationToken retrieveToken(String token);
     void removeTokenFromDB(VerificationToken token);
 }
