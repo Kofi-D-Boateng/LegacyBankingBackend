f37dfdc23adcc97df8707c4abd6fffc9e4c2bb6c
Kofi Boateng <kdboat2@gmail.com>
2022-09-08 14:31:09 -0500

    Fixed annotation

--- src/main/java/com/legacybanking/legacyBankingAPI/LegacyBankingApiApplication.java
+++ src/main/java/com/legacybanking/legacyBankingAPI/LegacyBankingApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableJpaRepositories("com.legacybanking.legacyBankingAPI.Repos")
 public class LegacyBankingApiApplication {
 
 	public static void main(String[] args) {
