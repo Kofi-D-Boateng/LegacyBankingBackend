be8beca02411646de49d9d72f9a6597a5e20fed9
Kofi Boateng <kdboat2@gmail.com>
2022-09-08 14:44:38 -0500

    added annotations to repo.

--- src/main/java/com/legacybanking/legacyBankingAPI/LegacyBankingApiApplication.java
+++ src/main/java/com/legacybanking/legacyBankingAPI/LegacyBankingApiApplication.java
@@ -1,14 +1,15 @@
 package com.legacybanking.legacyBankingAPI;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @EnableJpaRepositories("com.legacybanking.legacyBankingAPI.Repos")
-
 public class LegacyBankingApiApplication {
 
 	public static void main(String[] args) {
