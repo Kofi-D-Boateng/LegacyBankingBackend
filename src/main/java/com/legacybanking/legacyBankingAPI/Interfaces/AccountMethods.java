c9646a303c8fe2870966176a05de9157ccc2408f
Kofi Boateng <84227601+Kofi-D-Boateng@users.noreply.github.com>
2022-12-13 21:42:34 -0600

    Interfaces (#25)
    * Updated versioning of dockerfile.
    
    * Created an interface for withdrawing and depositing into an account.

---
+++ src/main/java/com/legacybanking/legacyBankingAPI/Interfaces/AccountMethods.java
@@ -0,0 +1,6 @@
+package com.legacybanking.legacyBankingAPI.Interfaces;
+
+public interface AccountMethods {
+    boolean deposit(Double amount);
+    boolean withdraw(Double amount);
+}
--- src/main/java/com/legacybanking/legacyBankingAPI/models/accounts/SavingsAccount.java
+++ src/main/java/com/legacybanking/legacyBankingAPI/models/accounts/SavingsAccount.java
@@ -1,5 +1,6 @@
 package com.legacybanking.legacyBankingAPI.models.accounts;
 
+import com.legacybanking.legacyBankingAPI.Interfaces.AccountMethods;
 import com.legacybanking.legacyBankingAPI.enums.BankAccountType;
 import com.legacybanking.legacyBankingAPI.models.abstractClass.Account;
 import com.legacybanking.legacyBankingAPI.models.customer.Customer;
@@ -12,7 +13,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 @Table(name = "savings_account")
-public class SavingsAccount extends Account {
+public class SavingsAccount extends Account implements AccountMethods {
     @Column(
             name = "interest_rate",
             columnDefinition = "Decimal(10,2) default '0.00'",
@@ -31,5 +32,21 @@ public class SavingsAccount extends Account {
         super(customer, accountNumber, routingNumber, bankAccountType, capital, isEnabled);
         this.interestRate = interestRate;
         this.maxAllowedContribution = maxAllowedContribution;
+    }
+
+    @Override
+    public boolean deposit(Double amount) {
+        if(amount > maxAllowedContribution){
+            return false;
+        }
+        maxAllowedContribution+=amount;
+        return true;
+    }
+
+    @Override
+    public boolean withdraw(Double amount) {
+        Double taxedAmount = (interestRate/100.00)*amount;
+        this.setCapital(this.getCapital()-taxedAmount);
+        return true;
     }
 }
